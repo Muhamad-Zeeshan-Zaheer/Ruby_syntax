d62a5f6aaeb2c8beda4738b8dc87527335279b0e
Muhammad Zeeshan Zahher <shan6818@gmail.com>
2025-01-28 17:08:12 +0500

    implemented methods array manipulation

--- string_manipulation.rb
+++ string_manipulation.rb
@@ -25,3 +25,17 @@ puts a.upcase
 puts "The string in downcase is"
 puts a.downcase
 
+
+
+q=[9,3,4,7,4,4,5,6]
+
+puts "implementation of Array rang is"
+puts q[2..6]
+puts "the value using -2 is"
+puts q[2..-2] # it start printing indexes from end 
+
+puts "the values using 0 ..-1"
+
+puts q[0..-1]
+
+
